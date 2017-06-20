eb3bc9404343ef180b5036df07f2ec41508d5eb4
Alexis <adoualle@gmail.com>
2017-06-20 20:23:33 +0200

    premier commit

---
+++ app/src/main/java/com/doualle/fredimob/FraisActivity.java
@@ -0,0 +1,13 @@
+package com.doualle.fredimob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FraisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frais);
+    }
+}
